5a12460f761a066b613ef6c2c6ccb9fe4e8415ae
Natasha <natashaelizedwards@gmail.com>
2016-03-13 19:29:24 -0400

    Add assignments

---
+++ chap2assign/myFavMovie.js
@@ -0,0 +1,6 @@
+//Natasha Edwards
+
+document.write("The Naval Fighter Weapons School called Top Gun by the flyers is where the best of the best pilots train. ");
+document.write("Aviator LT Pete 'Maverick' Mitchell and his Radar Intercept Officer LTJG Nick 'GOOSE' Bradshaw get their dream shot to compete for best in class. ");
+document.write("Maverick's reckless attitude and arrogance put him at odds with the other pilots. ");
+document.write("A civilian instructor, Charlotte Blackwood, catches his attention that carries into a romance. ");
